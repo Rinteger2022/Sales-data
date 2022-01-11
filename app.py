4f07840fd32482dff3296322b7eef4089421b440
Rinteger2022 <97272565+Rinteger2022@users.noreply.github.com>
2022-01-11 18:49:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ import os
 bearer_token = os.environ.get('BEARER_TOKEN')
 headers = {"Authorization": "Bearer {}".format(bearer_token)}
 
-url = "https://72ab3c1c126c24365a51553f67e1db84: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@acelance.myshopify.com/admin/api/2022-01/orders/count.json?status=any&updated_at_min="start_date"&updated_at_max="end_date
+url = "https://72ab3c1c126c24365a51553f67e1db84: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@acelance.myshopify.com/admin/api/2022-01/orders/count.json?status=any&updated_at_min="+str(start_date)"&updated_at_max="+str(end_date)
 response = requests.request("GET", url, headers=headers).json()
  
