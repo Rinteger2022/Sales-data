3cb92e6ca922ee52f7083bfca7f7c6417944c6bb
Rinteger2022 <97272565+Rinteger2022@users.noreply.github.com>
2022-01-11 18:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ end_date = st.date_input('End date', tomorrow)
  #   st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
 #else:
   #  st.error('Error: End date must fall after start date.')
+
+import requests
+import pandas as pd
+import os
+
+bearer_token = os.environ.get('BEARER_TOKEN')
+headers = {"Authorization": "Bearer {}".format(bearer_token)}
+
+url = "https://72ab3c1c126c24365a51553f67e1db84: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@acelance.myshopify.com/admin/api/2022-01/orders/count.json?status=any&updated_at_min="start_date"&updated_at_max="end_date
+response = requests.request("GET", url, headers=headers).json()
+ 
