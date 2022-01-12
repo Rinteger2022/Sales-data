de4c2f6c7ef2f47b1758d612bf77debabc8ed720
Rinteger2022 <97272565+Rinteger2022@users.noreply.github.com>
2022-01-12 15:51:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import datetime
 import timedelta
+from datetime import datetime,timedelta
+import requests
+import pandas as pd
+import os
+
 
 today = datetime.date.today()
 tomorrow = today + datetime.timedelta(days=1)
@@ -11,15 +16,16 @@ end_date = st.date_input('End date')
 #else:
   #  st.error('Error: End date must fall after start date.')
 
-import requests
-import pandas as pd
-import os
-st.write(end_date)
+currentTimeDate = end_date - timedelta(days=1)
+currentTime = currentTimeDate.strftime('%Y-%m-%d')
+st.write(currentTime)
+
+
 #a=end_date + timedelta(days = 1)
 bearer_token = os.environ.get('BEARER_TOKEN')
 headers = {"Authorization": "Bearer {}".format(bearer_token)}
 url_1="https://72ab3c1c126c24365a51553f67e1db84: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@acelance.myshopify.com/admin/api/2022-01/orders/count.json?status=any&created_at_min="+str(start_date)
-url_2=url_1+"&created_at_max="+str(end_date)
+url_2=url_1+"&created_at_max="+str(currentTime)
 url = url_2
 response = requests.request("GET", url, headers=headers).json()
 
@@ -29,7 +35,4 @@ if start_date <= end_date:
 # a=url.json()["count"]
  #st.write(str(a))
 
-from datetime import datetime,timedelta
-currentTimeDate = end_date - timedelta(days=1)
-currentTime = currentTimeDate.strftime('%Y-%m-%d')
-st.write(currentTime)
+
