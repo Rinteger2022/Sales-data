3d0df0f4513aec95104197c527946cde1cc0c20a
Rinteger2022 <97272565+Rinteger2022@users.noreply.github.com>
2022-06-30 17:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,18 @@
 import streamlit as st
-import datetime
-import timedelta
-from datetime import datetime,timedelta
-import requests
 import pandas as pd
-import os
+import numpy as np
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+
+st.title("Sentiment Analysis of Tweets about US Airlines")
+st.sidebar.title("Sentiment Analysis of Tweets about US Airlines")
+
+st.markdown("Product Intelligent ")
+st.sidebar.markdown("Inventory And Product Performance")
 
 
-#today = datetime.date.today()
-#tomorrow = today + datetime.timedelta(days=1)
-start_date = st.date_input('Start date')
-end_date = st.date_input('End date')
-#if start_date < end_date:
- #   st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
-#else:
-  #  st.error('Error: End date must fall after start date.')
-
-currentTimeDate = end_date + timedelta(days=1)
-currentTime = currentTimeDate.strftime('%Y-%m-%d')
-#st.write(currentTime)
-
-
-#a=end_date + timedelta(days = 1)
-bearer_token = os.environ.get('BEARER_TOKEN')
-headers = {"Authorization": "Bearer {}".format(bearer_token)}
-url_1="https://72ab3c1c126c24365a51553f67e1db84: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@acelance.myshopify.com/admin/api/2022-01/orders/count.json?status=any&created_at_min="+str(start_date)
-url_2=url_1+"&created_at_max="+str(currentTime)
-url = url_2
-response = requests.request("GET", url, headers=headers).json()
-
-if start_date <= end_date:
- st.write(response)
- st.write(url)
-# a=url.json()["count"]
- #st.write(str(a))
-
-
+DATA_URL = ("https://docs.google.com/spreadsheets/d/e/2PACX-1vShThnh_A7EQokSMvs1MKzDDzpZp13tOI2d9AjSGoxYgVYOcpgkwqhQD0tQT5pR8M3gOx7YZn9lS4Tr/pub?gid=1172464903&single=true&output=csv")
+DATA_URL_1 = ("https://docs.google.com/spreadsheets/d/e/2PACX-1vShThnh_A7EQokSMvs1MKzDDzpZp13tOI2d9AjSGoxYgVYOcpgkwqhQD0tQT5pR8M3gOx7YZn9lS4Tr/pub?gid=995490562&single=true&output=csv")
+@st.cache(persist=True)
+st.bar_chart(DATA_URL)
