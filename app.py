9d5f3440c9c45840a5e9c48a912464796f68a5b8
Rinteger2022 <97272565+Rinteger2022@users.noreply.github.com>
2022-06-30 18:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,8 @@ DATA_URL = ("https://docs.google.com/spreadsheets/d/e/2PACX-1vShThnh_A7EQokSMvs1
 #st.subheader(‘Weekly Demand Data’)
 #st.write(weekly_data)
 #Bar Chart
-st.bar_chart(DATA_URL["Reach"])
+#histogram
+df = pd.DataFrame(DATA_URL[:200], columns = ["Reach","Impressions","Frequency"])
+df.hist()
+plt.show()
+st.pyplot()
